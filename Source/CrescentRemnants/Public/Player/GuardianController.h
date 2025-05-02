7e74c378c2bcf897345d28d66bfbc06c6e0fc945
Benjamin Alexander <146328131+Benjameepmeep@users.noreply.github.com>
2025-05-02 11:29:11 +0200

    Cleaning up GuardianController

--- Source/CrescentRemnants/Public/Player/GuardianController.h
+++ Source/CrescentRemnants/Public/Player/GuardianController.h
@@ -5,25 +5,20 @@
 #include "Player/GuardianCharacter.h"
 #include "GuardianController.generated.h"
 
+class UInputMappingContext;
+
 /**
- * 
+ * AGuardianController is a custom player controller class designed for controlling the Guardian character in the game.
+ * This class sets up an enhanced input system for player interaction, defines input mappings, and manages the player's possession behaviour.
  */
 UCLASS()
 class CRESCENTREMNANTS_API AGuardianController : public APlayerController
 {
 	GENERATED_BODY()
-
+	
 	/** InputMappingContext (IMC) for the Guardian */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
 	UInputMappingContext* IMC_Guardian;
-	
-	virtual void SetupInputComponent() override;
-	
-	virtual void OnPossess(APawn* InPawn) override;
-	
-	void BindInputs(AGuardianCharacter* GuardianCharacter, UEnhancedInputComponent* EnhancedInputComponent);
-
-protected:
 
 	/** GuardianMove Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
@@ -48,6 +43,10 @@ protected:
 	/** GuardianEscape Input Action - Pause and Unpause */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
 	UInputAction* IA_Escape;
+	
+	virtual void SetupInputComponent() override;
+	virtual void OnPossess(APawn* InPawn) override;
+	void BindInputs(AGuardianCharacter* GuardianCharacter, UEnhancedInputComponent* EnhancedInputComponent);
 
 public:
 
@@ -61,15 +60,6 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Input")
 	UEnhancedInputComponent* GetEnhancedInputComponent() const { return Cast<UEnhancedInputComponent>(InputComponent); }
-
-	/**
-		 * FORCEINLINE forces the compiler to inline the function, but it increases the size of it.
-		 * Ideal for very small, frequently called getters, to reduce function call overhead. These just return a pointer.
-		 * Big functions would leave copies of inlined code everywhere...
-		 * For example:
-		 * Normal Function: 1x 20 bytes = 20 bytes total (just one function in memory).
-		 * 10 Inline Functions: 10x 20 bytes = 200 bytes total (10 separate copies!).
-		*/
 	
 	// Getter function for the mapping context
 	UFUNCTION(BlueprintCallable, Category = "Input")
