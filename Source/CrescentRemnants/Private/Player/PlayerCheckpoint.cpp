d645802bcb135f241582cb09840f1738ba50d1cf
Benjamin Alexander <146328131+Benjameepmeep@users.noreply.github.com>
2025-05-02 18:31:53 +0200

    PlayerCheckpoint Clean-up
    Removed redundant code and overall cleaned it up!
    
    Should be commented right and overall work nice. 👍

--- Source/CrescentRemnants/Private/Player/PlayerCheckpoint.cpp
+++ Source/CrescentRemnants/Private/Player/PlayerCheckpoint.cpp
@@ -2,117 +2,154 @@
 
 
 #include "Player/PlayerCheckpoint.h"
-
-#include "Components/PointLightComponent.h"
 #include "GameFramework/Character.h"
 
+/** Worth knowing is that any class deriving off of AWorldObject runs its constructor to initialise components and apply
+ * a default set of variables. The C++ implementation of doing things; the back-up plan, or Plan B.
+ *
+ * All components initialised here in the constructor will be inherited and already initialised for subclasses.
+ * Inherited classes should only add new components or modify the existing components.
+ */
 APlayerCheckpoint::APlayerCheckpoint()
 {
 	// Set this actor to call Tick() every frame. Set to false as default.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
-	// Setting default bools
-	bOneTimeUseOnly = true;
+	// Override parent class defaults as PlayerCheckpoint constructor defaults.
+	bIsActivated = false;    // Checkpoints are not activated by default.
+	bIsPickup = false;       // This isn't a regular pickup
+	bIsInteractable = true;  // Player interacts with checkpoints.
+	bEnableFloating = false; // Checkpoints should not float.
+	bIsLightOn = false;      // Checkpoints should not have lights on until activated.
 
-	// Setting DebugColor and LightColour
+	ObjectMeshScale = FVector(1.75f, 1.75f, 1.75f); // Default Mesh Scale for Checkpoints.
+
+	CapsuleLocationOffset = FVector(0.f, 0.f, 0.f); // Default Capsule Location for Checkpoints.
+	CapsuleHalfHeight = 300.f; // Default Capsule HalfHeight for Checkpoints.
+	CapsuleRadius = 300.f;	// Default Capsule Radius for Checkpoints.
+
+	ObjectLightLocation = FVector(0.f, 0.f, 0.f); // Default LightLocation for Checkpoints.
+	LightIntensity = 3000.f; // Default LightIntensity for Checkpoints.
+	LightAttenuationRadius = 1000.f; // Default LightAttenuationRadius for Checkpoints.
+	LightSourceRadius = 500.f; // Default LightSourceRadius for Checkpoints.
+
+	// Setting DebugColor and LightColour to Magenta / Purple, befitting the checkpoint.
 	DebugColour = FColor::Magenta;
 	LightColour = FColor::Purple;
-}
 
-bool APlayerCheckpoint::UseWorldObjectAssetSettings()
-{
-	if (Super::UseWorldObjectAssetSettings())
+// The below code is just loading the right ObjectMesh and ObjectMaterial "from scratch via C++", as a plan B.
+// Plan A involves using DataAssets to load and run meshes, but since that is Unreal Engine stuff and not 100% C++, this
+// is to prove it can be done fully in C++ too, while also serving as failsafe.
+	
+	// Using a local variable, to try loading the mesh and assign it to the instance variable ObjectMesh.
+	TSoftObjectPtr<UStaticMesh> PlayerCheckpointMesh = TSoftObjectPtr<UStaticMesh>
+	(FSoftObjectPath(TEXT("/Game/OurAssets/FaeCircle.FaeCircle")));
+
+	// Have we found the mesh, and is it loaded in time?
+	if (PlayerCheckpointMesh.IsValid())
 	{
-		Super::UseWorldObjectAssetSettings();
+		ObjectMesh = PlayerCheckpointMesh.Get();
 	}
 	else
 	{
-		return false;
+		// Try loading the UStaticMesh, and this works, assign it. If not, run a log warning.
+		if (UStaticMesh* LoadedCheckpointMesh = PlayerCheckpointMesh.LoadSynchronous())
+		{
+			ObjectMesh = LoadedCheckpointMesh;
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("❌ Failed to lazily load PlayerCheckpointMesh in %s!"), *GetClass()->GetName());
+		}
 	}
 
-	bIsPickup = SettingsAsset->bIsPickup;
-	bIsInteractable = SettingsAsset->bIsInteractable;
-	bIsActivated = SettingsAsset->bIsActivated;
-	
-	if (ObjectCapsuleComp)
+	// Loading MeshMaterialInterface, as with the ObjectMesh and the SettingsAsset.
+	TSoftObjectPtr<UMaterialInterface> PlayerCheckpointMaterial = TSoftObjectPtr<UMaterialInterface>
+	(FSoftObjectPath(TEXT("/Game/Materials/M_Floating_Platform.M_Floating_Platform")));
+
+	// Have we found the material, and is it loaded in time?
+	if (PlayerCheckpointMaterial.IsValid())
 	{
-		ObjectCapsuleComp->ShapeColor = DebugColour;
+		ObjectMaterial = PlayerCheckpointMaterial.Get();
 	}
-	return true;
-}
-
-void APlayerCheckpoint::BeginPlay()
-{
-	Super::BeginPlay();
-}
-
-void APlayerCheckpoint::Float()
-{
-	float Time = GetWorld()->TimeSeconds;
-	float Offset = FMath::Sin(Time * FloatingSpeed) * FloatingDistance;
-	
-	FVector NewLocation = InitialLocation + FVector(0.f, 0.f, Offset);
-	SetActorLocation(NewLocation);
-}
-
-
-void APlayerCheckpoint::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-	if (bIsActivated)
+	else
 	{
-		// TODO: Run a line from here to the GuardianCharacter, or do some other system to help track which checkpoint to use)
+		// Try loading the UMaterialInterface, and this works, assign it. If not, run a log warning.
+		if (UMaterialInterface* LoadedCheckpointMaterial = PlayerCheckpointMaterial.LoadSynchronous())
+		{
+			ObjectMaterial = LoadedCheckpointMaterial;
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("❌ Failed to lazily load PlayerCheckpointMaterial in %s!"), *GetClass()->GetName());
+		}
+	}
+
+	// Assigning the mesh and the material to the StaticMeshComponent if the mesh is valid.
+	if (ObjectMesh.IsValid() && ObjectMeshComp->IsValidLowLevel())
+	{
+		// If valid, set the mesh to the component
+		ObjectMeshComp->SetStaticMesh(ObjectMesh.Get());
+		// Pickups shouldn't have MeshCollision enabled (derived from AWorldObject). This saves on performance in-game.
+		ObjectMeshComp->SetCollisionProfileName(AWorldObject::GetMeshCollisionTag());
+
+		// Set Mesh Rotation and Scale if a mesh is found
+		ObjectMeshComp->SetWorldRotation(ObjectMeshRotation);
+		ObjectMeshComp->SetWorldScale3D(ObjectMeshScale);
+
+		if (PlayerCheckpointMaterial.IsValid())
+		{
+			// If valid, set the material to the component
+			ObjectMeshComp->SetMaterial(0, ObjectMaterial.Get());
+		}
 	}
 }
-
-#if WITH_EDITOR
-
-void APlayerCheckpoint::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
-{
-	Super::PostEditChangeProperty(PropertyChangedEvent);
-}
-
-#endif
 
 void APlayerCheckpoint::PlayerEntersInteractable()
 {
+	if (bDebugEnabled)
+	{
+		// Any light toggling and debug printing happen in the superclass function.
+		Super::PlayerEntersInteractable(); 
+	}
+
+	// Usually for checkpoints - if activated and toggled to be one-time use, don't do anything during player overlaps.
 	if (bIsActivated && bOneTimeUseOnly) return;
-	
-	Super::PlayerEntersInteractable();
+
+	// We don't want to run the superclass function twice if debug is enabled when we have already run it once above.
+	if (!bDebugEnabled) 
+	{
+		Super::PlayerEntersInteractable();
+	}
 
 	if (!bIsActivated)
 	{
-		// Ensuring lights are on. We want this for the checkpoint.
-		bIsLightOn = true;
-		ObjectPointLightComp->SetVisibility(bIsLightOn);
-		
-		// Just making a random colour for fun. :)
-		ObjectPointLightComp->LightColor = FColor::MakeRandomColor();
-		
-		// the object is now activated. If oneTimeUse is enabled, this stays true.
+		// The object is now activated. If oneTimeUse is enabled, this stays true.
 		bIsActivated = true;
 	}
 	else
 	{
+		// Flagging the object as inactive / deactivated.
 		bIsActivated = false;
 	}
-	
-	// TODO: Add functionality for when player first overlaps with Interactable.
 }
 
-// Commenting out the names in the parameters to silence warnings (we're not using 'em).
 void APlayerCheckpoint::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 										UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	if (OtherActor && Cast<ACharacter>(OtherActor))
+	// Check if it's ACharacter
+	if (const ACharacter* Character = Cast<ACharacter>(OtherActor))
 	{
-		if (bDebugEnabled)
+		// Check if this character is controlled by a player (eliminate enemy AI from the logic)
+		if (Cast<APlayerController>(Character->GetController()))
 		{
-			FString objectName = this->GetName();
+			if (bDebugEnabled)
+			{
+				FString objectName = this->GetName();
 	
-			check(GEngine != nullptr);
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, DebugColour, TEXT("Player leaves checkpoint! ... remember me as " + objectName));
+				check(GEngine != nullptr);
+				GEngine->AddOnScreenDebugMessage(-1, 5.0f, DebugColour, TEXT("Player leaves checkpoint! ... remember me as " + objectName));
+			}
 		}
 	}
 }
