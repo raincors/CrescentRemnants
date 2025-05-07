4e0adaca712ffc78354163fc20f93c0fd19d2e2d
Benjamin Alexander <146328131+Benjameepmeep@users.noreply.github.com>
2025-05-07 18:46:04 +0200

    Less air movement on the Guardian

--- Source/CrescentRemnants/Private/Player/GuardianCharacter.cpp
+++ Source/CrescentRemnants/Private/Player/GuardianCharacter.cpp
@@ -145,7 +145,7 @@ AGuardianCharacter::AGuardianCharacter()
 
 		// Jumping
 		GetCharacterMovement()->JumpZVelocity = GuardianJumpStrength; // Jumping speed
-		GetCharacterMovement()->AirControl = 0.5f; // Control mid-air
+		GetCharacterMovement()->AirControl = 0.35f; // Control mid-air
 		JumpMaxCount = 2; // We don't want more than 2 jumps.
 	}
 
